d21ca8681f9c111b7d2c60ad3a62a9b0a79a116c
min8802 <ww330@naver.com>
2024-05-27 09:29:26 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <div className="bg-red-100">Hi </div>
+  return (
+    <div className="bg-red-100 min-h-screen mx-auto">
+      <Header/>
+      <div>
+        <img className="w-full h-[460px] object-cover" src="../images/zigbangWeb_heroImg_1440.jpg"/>
+      </div>
+    </div>
+  ) 
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+const Header = () => {
+  return (
+      <div className="bg-white h-[80px] flex justify-between items-center">
+          <div className="w-[226px] h-[79px] flex items-center flex-shrink-0">
+              <img className="pl-[20px] pr-[8px] pt-[15px] pb-[14px] cursor-pointer" src="../images/zigbangWeb_horizontal_on-line_new.png" />
+          </div>
+          <div className="w-[640px] h-[77px] flex flex-shrink-0">
+              <ul className="flex space-x-4 flex-shrink-0">
+                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0">
+                      <span className="top-big-font">아파트</span>
+                      <span className="top-small-font">매매/전월세/신축분양</span>
+                  </li>
+                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0">
+                      <span className="top-big-font">빌라, 투룸+</span>
+                      <span className="top-small-font">신축분양/매매/전월세</span>
+                  </li>
+                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0">
+                      <span className="top-big-font">원룸</span>
+                      <span className="top-small-font">전월세</span>
+                  </li>
+                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0">
+                      <span className="top-big-font">오피스텔</span>
+                      <span className="top-small-font">도시형생활주택/전월세</span>
+                  </li>
+                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0">
+                      <span className="top-big-font mr-0">상가</span>
+                      <span className="top-small-font">임대/매매</span>
+                  </li>
+              </ul>
+          </div>
+          <div className="flex items-center flex-shrink-0">
+              <button className="min-w-[106px] h-[30px] rounded bg-[#EEEEEE] px-2 pt-[5px] pb-[6px] top-small-font border border-gray-200">
+                  로그인 및 회원가입
+              </button>
+              <div className="ml-[15px] pl-[13px] pr-[21px] pt-[21px] pb-5">
+                  <img className="w-[126px] h-[38px]" src="../images/ceo_ad_img_new.png" />
+              </div>
+          </div>
+      </div>
+  );
+};
+
+export default Header;
