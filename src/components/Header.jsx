02a0c64ec9f2067237de5b9f9d3dcef829eeaff8
min8802 <ww330@naver.com>
2024-05-28 00:34:58 +0900

    top dropdown transition edit-ing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 const Header = () => {
 
-    const changeColor = (e) => {
+    const [isHover, setIsHover] = useState(false);
+
+    const changeColor = (e, dropdownId) => {
         const liEl = e.currentTarget;
         console.log(liEl);
         const spanEl = liEl.querySelectorAll('span')
@@ -10,10 +14,15 @@ const Header = () => {
         });
         console.log(e);
 
+        const subDropdownEl = document.getElementById(dropdownId);
+        subDropdownEl.style.display = "block"
+        
+
         liEl.addEventListener('mouseout', () => {
             spanEl.forEach(span => {
                 span.style.color = ""; // 기본값으로 변경
             });
+            subDropdownEl.style.display = "none"
         });
     };
 
@@ -24,26 +33,55 @@ const Header = () => {
           </div>
           <div className="min-w-[640px] h-[77px] flex flex-1 ml-[10px]">
               <ul className="flex">
-                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0" onMouseOver={(e) => changeColor(e)}>
-                      <span className="top-big-font">아파트</span>
-                      <span className="top-small-font">매매/전월세/신축분양</span>
-                  </li>
-                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0" onMouseOver={(e) => changeColor(e)}>
-                      <span className="top-big-font">빌라, 투룸+</span>
-                      <span className="top-small-font">신축분양/매매/전월세</span>
-                  </li>
-                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0" onMouseOver={(e) => changeColor(e)}>
-                      <span className="top-big-font">원룸</span>
-                      <span className="top-small-font">전월세</span>
-                  </li>
-                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0" onMouseOver={(e) => changeColor(e)}>
-                      <span className="top-big-font">오피스텔</span>
-                      <span className="top-small-font">도시형생활주택/전월세</span>
-                  </li>
-                  <li className="top-li-style flex flex-col justify-center items-start flex-shrink-0" onMouseOver={(e) => changeColor(e)}>
-                      <span className="top-big-font mr-0">상가</span>
-                      <span className="top-small-font">임대/매매</span>
-                  </li>
+                <li onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)} className="top-li-style flex flex-col justify-center items-start flex-shrink-0 relative" 
+                >
+                    <span className="top-big-font">아파트</span>
+                    <span className="top-small-font">매매/전월세/신축분양</span>
+                    <div id="" className={`top-dropdown-style transition duration-1000 ${isHover ? "bg-red-100 opacity-100" : "bg-blue-100 opacity-0"}`}>
+                        <div className="top-subDropdown-style">매매/전*월세</div>
+                        <div className="top-subDropdown-style">신축분양</div>
+                        <div className="top-subDropdown-style">인구흐름</div>
+                    </div>
+                </li>
+                
+                <li className="top-li-style flex flex-col justify-center items-start relative" onMouseOver={(e) => changeColor(e, 'top-dropdown-2')}>
+                    <span className="top-big-font">빌라, 투룸+</span>
+                    <span className="top-small-font">신축분양/매매/전월세</span>
+                    <div id="top-dropdown-2" className="top-dropdown-style hidden">
+                        <div className="top-subDropdown-style">빌라, 투룸 찾기</div>
+                        <div className="top-subDropdown-style">찜한 매물</div>
+                        <div className="top-subDropdown-style">빌라 내놓기(전/월세만)</div>
+                    </div>
+                </li>
+                <li className="top-li-style flex flex-col justify-center items-start relative" onMouseOver={(e) => changeColor(e, 'top-dropdown-3')}>
+                    <span className="top-big-font">원룸</span>
+                    <span className="top-small-font">전월세</span>
+                    <div id="top-dropdown-3" className="top-dropdown-style hidden">
+                        <div className="top-subDropdown-style">방 찾기</div>
+                        <div className="top-subDropdown-style">찜한 매물</div>
+                        <div className="top-subDropdown-style">방 내놓기(전/월세만)</div>
+                    </div>
+                </li>
+                <li className="top-li-style flex flex-col justify-center items-start relative" onMouseOver={(e) => changeColor(e, 'top-dropdown-4')}>
+                    <span className="top-big-font">오피스텔</span>
+                    <span className="top-small-font">도시형생활주택/전월세</span>
+                    <div id="top-dropdown-4" className="top-dropdown-style hidden">
+                        <div className="top-subDropdown-style">오피스텔 찾기</div>
+                        <div className="top-subDropdown-style">찜한 매물</div>
+                        <div className="top-subDropdown-style">오피스텔 내놓기(전/월세만)</div>
+                    </div>
+                </li>
+                <li className="top-li-style flex flex-col justify-center items-start relative" onMouseOver={(e) => changeColor(e, 'top-dropdown-5')}>
+                    <span className="top-big-font mr-0">상가</span>
+                    <span className="top-small-font">임대/매매</span>
+                    <div id="top-dropdown-5" className="top-dropdown-style hidden">
+                        <div className="top-subDropdown-style">상가 * 점포</div>
+                        <div className="top-subDropdown-style">사무실</div>
+                        <div className="top-subDropdown-style">공유오피스</div>
+                        <div className="top-subDropdown-style">찜한 매물</div>
+                        <div className="top-subDropdown-style">상가 내놓기</div>
+                    </div>
+                </li>
               </ul>
           </div>
           <div className="flex items-center min-w-[287px]">
